06158aeff7657b3664e1f2b6f02b521d2392129e
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-08-16 11:48:55 -0400

    Installed axios. Called the API and received person data back. Added a new component NewCharacter to render the HTML elements for each character.

---
+++ starwars/src/components/NewCharacter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const NewCharacter = props => {
+  return (
+    <div>
+      <p>{props.name}</p>
+    </div>
+  );
+};
+
+export default NewCharacter;
